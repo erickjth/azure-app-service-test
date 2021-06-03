5160058e715dd6f86dcaafb8d9170e465d3ecf91
Erick Torres <erickt@digintent.com>
2021-06-03 11:22:19 -0500

    Add minimal server

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 require("dotenv").config();
+const http = require("http");
 const { Connection } = require("tedious");
+
+const port = process.env.PORT || 8080;
 
 const connection = new Connection({
   server: process.env.SQL_SERVER_NAME,
@@ -31,12 +34,25 @@ function connect() {
 const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 async function main() {
-  let isRetrying = false;
+  let attemps = 0;
+  let connected = false;
+
+  http
+    .createServer(function (req, res) {
+      res.writeHead(200, { "Content-Type": "text/plain" });
+      res.write("Hello World - this is node.js\n");
+      res.write("Date on server: " + now.toGMTString());
+      res.write("Connect status: ", { connected });
+      res.end("\nbye!");
+    })
+    .listen(port, "");
+
+  console.log("Server running at port: " + port);
 
   while (true) {
     try {
-      if (isRetrying) {
-        console.log("Retrying....");
+      if (attemps > 0) {
+        console.log(`Retrying (attemp #${attemps})....`);
       }
 
       console.log("Connect with env", process.env);
@@ -49,7 +65,7 @@ async function main() {
     } catch (e) {
       console.log("Error trying to connect", e);
       console.log("Retrying in 5ms");
-      isRetrying = true;
+      attemps++;
       await sleep(5000);
     }
   }
